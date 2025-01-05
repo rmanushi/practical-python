fbbced4b084cba30e8789da61a3420bb7ab9e96a
rmanushi <reimanushi@gmail.com>
2025-01-05 18:02:02 +0100

    Finished Tutorial 1

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,22 @@
 # bounce.py
 #
 # Exercise 1.5
+
+#Variables.
+ball_bounce_back_rate = 0.6
+
+initial_height = 100
+
+num_bounce = 0
+
+while num_bounce < 10:
+    if num_bounce == 0:
+        new_height = ball_bounce_back_rate * initial_height
+        num_bounce = num_bounce + 1
+        print(round(new_height,4))
+    else:
+        new_height = new_height * ball_bounce_back_rate
+        num_bounce = num_bounce + 1
+        print(round(new_height,4))
+
+
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,32 @@
 # pcost.py
 #
 # Exercise 1.27
+
+import os
+
+def portfolio_cost(filename):
+    cwd_path =  os.getcwd()
+    full_path = cwd_path + '\Work\Data' + filename
+    #print(full_path)
+
+    file = open(full_path, 'rt')
+
+    headers = next(file).split(',')
+
+    total = 0
+
+    #full file content display
+    print(headers)
+    for line in file:
+        row = line.split(',')
+        #caclculate total cost for all shares
+        try:
+            total += (int(row[1]) * round(float(row[2].replace('\n','')),2))
+        except ValueError:
+            print('Bad row:', row)
+    
+        print(row)
+
+    print('Total Cost: ',total)
+
+portfolio_cost('\portfolio.csv')
